743f8ddcc1171602f0b975b18d4ebf866d5fed15
Gajus Kuizinas <gk@anuary.com>
2014-11-07 17:52:57 +0200

    Not production safe.

--- dist/brim.js
+++ dist/brim.js
@@ -1,5 +1,5 @@
 /**
- * @version 1.0.0
+ * @version 1.0.1
  * @link https://github.com/gajus/brim for the canonical source repository
  * @license https://github.com/gajus/brim/blob/master/LICENSE BSD 3-Clause
  */
