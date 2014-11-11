0b4cfa21d42abc09794ac19b892872302ae7b279
Gajus Kuizinas <gk@anuary.com>
2014-11-11 21:45:01 +0200

    Various optimisation.

--- dist/brim.js
+++ dist/brim.js
@@ -1,5 +1,5 @@
 /**
- * @version 1.0.3
+ * @version 1.0.4
  * @link https://github.com/gajus/brim for the canonical source repository
  * @license https://github.com/gajus/brim/blob/master/LICENSE BSD 3-Clause
  */
