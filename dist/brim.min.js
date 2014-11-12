5f4c214ce343073518f7952e7476d92f299e7be6
Gajus Kuizinas <gk@anuary.com>
2014-11-12 10:01:06 +0200

    Stable.

--- dist/brim.min.js
+++ dist/brim.min.js
@@ -1,5 +1,5 @@
 /**
- * @version 1.0.4
+ * @version 1.0.5
  * @link https://github.com/gajus/brim for the canonical source repository
  * @license https://github.com/gajus/brim/blob/master/LICENSE BSD 3-Clause
  */
