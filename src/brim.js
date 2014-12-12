7de4058deb26d3cc25fc366ea1d7a1ed12bb3801
Daniel Schmidt <dschmidt@weluse.de>
2014-12-12 22:25:56 +0100

    Made eshint happy
    Fixed some eshint errors, commented out some code which may be deleted IMHO.

--- src/brim.js
+++ src/brim.js
@@ -1,3 +1,6 @@
+/* global document,gajus */
+'use strict';
+
 var Brim,
     Sister = require('sister');
 
@@ -6,7 +9,7 @@ Brim = function Brim (config) {
         player = {},
         viewport,
         eventEmitter;
-    
+
     if (!(this instanceof Brim)) {
         return new Brim(config);
     }
@@ -41,21 +44,23 @@ Brim = function Brim (config) {
 
         // Disable window scrolling when in minimal view.
         // @see http://stackoverflow.com/a/26853900/368691
-        (function () {
-            var firstMove;
 
-            global.document.addEventListener('touchstart', function (e) {
-                firstMove = true;
-            });
+        // FIXME: As far as I know this isn't needed anymore, may be deleted
+        // (function () {
+        //     var firstMove;
 
-            global.document.addEventListener('touchmove', function (e) {
-                if (viewport.isMinimalView() && firstMove) {
-                    //e.preventDefault();
-                }
+        //     global.document.addEventListener('touchstart', function (e) {
+        //         firstMove = true;
+        //     });
 
-                firstMove = false;
-            });
-        } ());
+        //     global.document.addEventListener('touchmove', function (e) {
+        //         if (viewport.isMinimalView() && firstMove) {
+        //             //e.preventDefault();
+        //         }
+
+        //         firstMove = false;
+        //     });
+        // })();
     };
 
     /**
@@ -81,7 +86,7 @@ Brim = function Brim (config) {
             player.mask.style.display = 'block';
 
             player.mask.style.width = global.innerWidth + 'px';
-            player.mask.style.height = (global.innerHeight * 2) + 'px';
+            player.mask.style.height = global.innerHeight * 2 + 'px';
 
             brim._repaintElement(player.mask);
         }
@@ -184,7 +189,6 @@ Brim = function Brim (config) {
         element.style.webkitTransform = 'translateZ(0)';
 
         element.style.display = 'none';
-        element.offsetHeight;
         element.style.display = 'block';
     };
 
